d2fd14a0ade84f23e48428174f17b85730e6ba75
matt <matt@matthew>
2020-10-20 08:38:05 +0100

    Log

--- src/main/java/bpdts/BpdtsApplication.java
+++ src/main/java/bpdts/BpdtsApplication.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
