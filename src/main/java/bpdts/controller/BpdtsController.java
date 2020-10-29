b92d8d7ad7d6d9941844fc3ca963a3f5dacc6696
matt <matt@matthew>
2020-10-29 07:39:17 +0000

    Tidy

--- src/main/java/bpdts/controller/BpdtsController.java
+++ src/main/java/bpdts/controller/BpdtsController.java
@@ -4,16 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
-import bpdts.exception.ResourceNotFoundException;
 import bpdts.model.User;
 import bpdts.service.UserService;
 import io.swagger.annotations.ApiOperation;
